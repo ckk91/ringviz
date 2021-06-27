a9381c78ea2bcc6c7c2b4cc11c12d4807e58b309
Chris K <hello@chris-k.at>
2021-06-27 20:18:07 +0200

    cleanup

---
+++ README.md
@@ -0,0 +1,13 @@
+# Ringviz - A ThreeJS Toy
+A **Ring** **Viz**ualization demo. Result of playing around with ThreeJS, FFT and sound. A personal learning experience of varying aesthetic quality. Code is pretty sphaget, but works-on-my-machine(tm) :spaghetti:
+
+## How to use
+- Find it here: 
+- Click play. Headphone warning!
+- Use mouse click and drag to look around
+
+## Credits
+- Song: Voxel Revolution, Kevin MacLeod (incompetech.com), Licensed under Creative Commons: By Attribution 3.0 License http://creativecommons.org/licenses/by/3.0/
+- ThreeJs: https://threejs.org/
+- ThreeJs Orbit Controls: https://threejs.org/docs/#examples/en/controls/OrbitControls
+- Stats.js performance monitor by mrdoob https://github.com/mrdoob/stats.js/
--- main.js
+++ main.js
@@ -1,21 +1,22 @@
 import * as THREE from './vendor/three.module.js'
-import {OrbitControls} from './vendor/OrbitControls.js'
+import { OrbitControls } from './vendor/OrbitControls.js'
 import Stats from './vendor/stats.module.js';
-let sound, camera,renderer, canvas, freqs, hiBall, rings, vizGroup, floaterRed, scene,analyser, floaterBlue;
+let sound, camera, renderer, canvas, freqs, hiBall, rings, vizGroup, floaterRed, scene, analyser, floaterBlue;
 let controls, floaterGreen, stats;
 let objects;
 
 init();
 requestAnimationFrame(render);
+
 function init() {
-    
-    canvas= document.getElementById('c');
-    renderer = new THREE.WebGLRenderer({canvas});
+
+    canvas = document.getElementById('c');
+    renderer = new THREE.WebGLRenderer({ canvas });
 
     objects = {
-        floaters:[],
+        floaters: [],
         speaker: {
-            rings:[],
+            rings: [],
             group: undefined
         },
         scene: undefined
@@ -23,33 +24,35 @@ function init() {
     const config = {
         cam: {
 
-        fov: 75,
-        aspect: 2,
-        near: 0.1,
-        far: 60,
-        zPos:25,
+            fov: 75,
+            aspect: 2,
+            near: 0.1,
+            far: 60,
+            zPos: 25,
         },
         scene: {
             color: 0x000000
         }
     };
-    stats= new Stats();
+
+    stats = new Stats();
     stats.showPanel(1);
     document.body.appendChild(stats.dom);
-    stats.dom.style.left='unset';
-    stats.dom.style.right='0%';
+    stats.dom.style.left = 'unset';
+    stats.dom.style.right = '0%';
 
     // cam setup
     camera = new THREE.PerspectiveCamera(
-        config.cam.fov,config.cam.aspect,config.cam.near,config.cam.far
+        config.cam.fov, config.cam.aspect, config.cam.near, config.cam.far
     );
     camera.position.z = config.cam.zPos;
 
     scene = new THREE.Scene();
-    scene.background=new THREE.Color(config.scene.color);
+    scene.background = new THREE.Color(config.scene.color);
     objects.scene = scene;
-    const boxMaterial= new THREE.MeshPhongMaterial({color: 0xa8a8a8});
-    boxMaterial.flatShading=true;
+
+    const boxMaterial = new THREE.MeshPhongMaterial({ color: 0xa8a8a8 });
+    boxMaterial.flatShading = true;
 
     // make speaker
     // create 15 rings, concentric, from outside in
@@ -57,9 +60,9 @@ function init() {
     rings = [];
     for (let i = 0; i < 15; i++) {
         var ring = new THREE.Mesh(
-                new THREE.TorusGeometry(1-(i/15)+.5*i,.2,16,16),
-                boxMaterial
-            );
+            new THREE.TorusGeometry(1 - (i / 15) + .5 * i, .2, 16, 16),
+            boxMaterial
+        );
         vizGroup.add(ring);
         rings.push(ring);
     }
@@ -69,47 +72,43 @@ function init() {
     hiBall = new THREE.Mesh(
         new THREE.SphereGeometry(.5),
         boxMaterial
-        );
-    // hiBall.add(new THREE.SpotLight(0xff0000));
-    // hiBall.add(new THREE.SpotLight(0x0000ff));
+    );
     vizGroup.add(hiBall);
-    // group everything
     scene.add(vizGroup)
 
 
-    floaterRed = new THREE.PointLight(0xff0000,1.5,20);
-    floaterRed.add(new THREE.Mesh(new THREE.SphereGeometry(.25), new THREE.MeshBasicMaterial({color:0xff0000})));
+    floaterRed = new THREE.PointLight(0xff0000, 1.5, 20);
+    floaterRed.add(new THREE.Mesh(new THREE.SphereGeometry(.25), new THREE.MeshBasicMaterial({ color: 0xff0000 })));
     scene.add(floaterRed);
-    floaterBlue = new THREE.PointLight(0x0000ff,1.5,20);
-    floaterBlue.add(new THREE.Mesh(new THREE.SphereGeometry(.25), new THREE.MeshBasicMaterial({color:0x0000ff})));
+    floaterBlue = new THREE.PointLight(0x0000ff, 1.5, 20);
+    floaterBlue.add(new THREE.Mesh(new THREE.SphereGeometry(.25), new THREE.MeshBasicMaterial({ color: 0x0000ff })));
     scene.add(floaterBlue);
-    floaterGreen = new THREE.PointLight(0x00ff00,1.5,20);
-    floaterGreen.add(new THREE.Mesh(new THREE.SphereGeometry(.25), new THREE.MeshBasicMaterial({color:0x00ff00})));
+    floaterGreen = new THREE.PointLight(0x00ff00, 1.5, 20);
+    floaterGreen.add(new THREE.Mesh(new THREE.SphereGeometry(.25), new THREE.MeshBasicMaterial({ color: 0x00ff00 })));
     scene.add(floaterGreen);
     objects.floaters = [floaterRed, floaterBlue, floaterGreen];
 
-    vizGroup.rotation.x=-Math.PI*.35;
-    vizGroup.rotation.y=Math.PI*.75;
-    // vizGroup.rotation.z=Math.PI*.7
-    vizGroup.position.z=-2
-    vizGroup.position.x=3
-    vizGroup.position.y=-4
+    vizGroup.rotation.x = -Math.PI * .35;
+    vizGroup.rotation.y = Math.PI * .75;
+    vizGroup.position.z = -2
+    vizGroup.position.x = 3
+    vizGroup.position.y = -4
 
     // let there be light
     const light = new THREE.DirectionalLight(
         0xFFFFFF, 1
     );
-    light.position.set(-1,2,4);
+    light.position.set(-1, 2, 4);
     scene.add(light);
 
     const room = new THREE.Mesh(
-        new THREE.BoxGeometry(30,30,50),
+        new THREE.BoxGeometry(30, 30, 50),
         new THREE.MeshPhongMaterial({
             color: 0xa0adaf,
             side: THREE.BackSide
         })
     );
-    room.receiveShadow=true;
+    room.receiveShadow = true;
     scene.add(room);
 
 
@@ -121,24 +120,19 @@ function init() {
     camera.add(listener);
     sound = new THREE.Audio(listener);
     const audioLoader = new THREE.AudioLoader();
-    // audioLoader.load('11. Motorhead.mp3',
-    // audioLoader.load('02 Scary Monsters And Nice Sprites.mp3',
-    // audioLoader.load('./1-10 Thiz Rox.mp3',
     audioLoader.load('./voxel_revolution.mp3',
-    // audioLoader.load('./audiocheck.net_sweep20-20klin.ogg',
-    // audioLoader.load('./phat-sketch-by-kevin-macleod-from-filmmusic-io.mp3',
-    buffer=>{
-        sound.setBuffer(buffer);
-        sound.setLoop(true);
-        sound.setVolume(0.5);
-    });
-    document.getElementById('play').addEventListener('click', ()=>sound.play());
+        buffer => {
+            sound.setBuffer(buffer);
+            sound.setLoop(true);
+            sound.setVolume(0.5);
+        });
+    document.getElementById('play').addEventListener('click', () => sound.play());
 
     analyser = new THREE.AudioAnalyser(sound, 32);
-    freqs =[];
-for (let i = 0; i < 16; i++) {
-    freqs.push(0);
-}
+    freqs = [];
+    for (let i = 0; i < 16; i++) {
+        freqs.push(0);
+    }
 
     controls = new OrbitControls(camera, renderer.domElement);
     controls.update();
@@ -152,14 +146,16 @@ function resizeRendererToDisplaySize(renderer) {
     const width = canvas.clientWidth;
     const height = canvas.clientHeight;
     const needResize = canvas.width !== width || canvas.heigth !== height;
+
     if (needResize) {
         renderer.setSize(width, height, false);
     }
+
     return needResize;
 }
 function norm(val, min, max) {  // normalize to f0-f1
-/** https://gist.github.com/Anthodpnt/aafeb0dc669fb9137dd0550b6f5d8630 */
-    return (val-min)/(max-min)
+    /** https://gist.github.com/Anthodpnt/aafeb0dc669fb9137dd0550b6f5d8630 */
+    return (val - min) / (max - min)
 }
 
 /** MAKE IT MOVE */
@@ -167,46 +163,49 @@ function norm(val, min, max) {  // normalize to f0-f1
 function render(time) {
     stats.begin();
     if (sound.isPlaying) {
-        
-    freqs = analyser.getFrequencyData();
+
+        freqs = analyser.getFrequencyData();
     }
 
-
     time *= 0.001;
+
     // make canvas according to viewport
     // might affect performance??? find out
     if (resizeRendererToDisplaySize(renderer)) {
-        
-    canvas = renderer.domElement;
-    camera.aspect = canvas.clientWidth / canvas.clientHeight;
-    camera.updateProjectionMatrix();
+
+        canvas = renderer.domElement;
+        camera.aspect = canvas.clientWidth / canvas.clientHeight;
+        camera.updateProjectionMatrix();
     }
 
 
-    const normBall = norm(freqs[freqs.length-1],0,254);
-    hiBall.position.z = normBall<=0.2?-norm(freqs[freqs.length-2],0,254)*6:-normBall*6;
+    const normBall = norm(freqs[freqs.length - 1], 0, 254);
+    hiBall.position.z = normBall <= 0.2 ? -norm(freqs[freqs.length - 2], 0, 254) * 6 : -normBall * 6;
     for (let i = 0; i < rings.length; i++) {
         const ring = rings[i];
-        const normed = norm(freqs[i],0,254);
-        ring.position.z=normed*(i+5);
-        ring.rotation.z=time*(i)*.125;
+        const normed = norm(freqs[i], 0, 254);
+        ring.position.z = normed * (i + 5);
+        ring.rotation.z = time * (i) * .125;
     }
     hiBall.rotation.x = time;
     hiBall.rotation.y = time;
-    // vizGroup.rotation.y=Math.sin(time)*.5;
-    // vizGroup.position.x=Math.sin(time)*8;
-    // vizGroup.position.z=Math.cos(time)*8;
-    floaterBlue.position.x = Math.cos(time)*8;
-    floaterBlue.position.y = Math.sin(time)*8;
-    floaterBlue.position.z = Math.cos(time)*8;
-    floaterRed.position.x = Math.cos(time)*8;
-    floaterRed.position.y = Math.cos(time)*8;
-    floaterRed.position.z = Math.sin(time)*8;
-    floaterGreen.position.x = Math.sin(time)*8;
-    floaterGreen.position.y = Math.cos(time)*8;
-    floaterGreen.position.z = Math.cos(time)*8;
+
+    floaterBlue.position.x = Math.cos(time) * 8;
+    floaterBlue.position.y = Math.sin(time) * 8;
+    floaterBlue.position.z = Math.cos(time) * 8;
+
+    floaterRed.position.x = Math.cos(time) * 8;
+    floaterRed.position.y = Math.cos(time) * 8;
+    floaterRed.position.z = Math.sin(time) * 8;
+
+    floaterGreen.position.x = Math.sin(time) * 8;
+    floaterGreen.position.y = Math.cos(time) * 8;
+    floaterGreen.position.z = Math.cos(time) * 8;
+
     controls.update();
     renderer.render(scene, camera);
     stats.end();
+
+    // and do it again!
     requestAnimationFrame(render);
 }
